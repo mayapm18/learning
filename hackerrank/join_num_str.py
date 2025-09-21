f84547eaaf188d6998e9d57aa18b250c2583a2de
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-21 19:16:25 +0300

    join the nums in str

---
+++ hackerrank/join_num_str.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+
+if __name__ == '__main__':
+    n = int(input())
+    s = ""
+    for x in range (1, n+1):
+        s = s+ str(x)
+    print(s)
