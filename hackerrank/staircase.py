4f79c5fc627b6fd647c8ce60f8b0171dba9df8ae
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-27 20:43:13 +0300

    stulba ot #

---
+++ hackerrank/staircase.py
@@ -0,0 +1,22 @@
+#https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n+1):
+        print(' '*(n-i) + '#'*i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
