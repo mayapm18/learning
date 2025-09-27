543eeccbb9cd13ef2d757512ee010ccc79fd1d7c
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-27 19:31:52 +0300

    sum big nums 2^31

---
+++ hackerrank/sum_big_num_arr.py
@@ -0,0 +1,33 @@
+#https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    k = 0
+    for a in ar:
+        k = k + a
+    return k
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
