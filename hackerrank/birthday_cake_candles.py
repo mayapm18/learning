89d63bac4009b29a3412a706c82009f6522e6b64
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-28 17:59:31 +0300

    ne raboti 4/9 failed

---
+++ hackerrank/birthday_cake_candles.py
@@ -0,0 +1,39 @@
+#http://hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    max_of_a = candles[0]
+    count_of_max = 0
+    for a in candles:
+        if a > max_of_a:
+            max_of_a = a
+            count_of_max = 1
+        if a == max_of_a:
+            count_of_max += 1
+            
+    return count_of_max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
