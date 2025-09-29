bda3379129e9e451ef522f0239d8cf7888fe1059
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-29 18:52:45 +0300

    xcx

---
+++ hackerrank/apple_xcx.py
@@ -0,0 +1,6 @@
+# I think the apple's rotten right to the core
+# From all the things passed down
+# From all the apples coming before
+# I split the apple down symmetrical lines
+# And what I find is kinda scary
+# Makes me just wanna drive
