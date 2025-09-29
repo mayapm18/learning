5268ef1c5013697100157bc54526d3fae01b3c6c
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-29 18:53:19 +0300

    uslovie

--- hackerrank/apple_xcx.py
+++ hackerrank/apple_xcx.py
@@ -1,6 +1,1 @@
-# I think the apple's rotten right to the core
-# From all the things passed down
-# From all the apples coming before
-# I split the apple down symmetrical lines
-# And what I find is kinda scary
-# Makes me just wanna drive
+# https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
