688737cf69296ff4f65dcd14d92f4d699562d9a0
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-29 23:22:26 +0300

    missing 0s :(

--- hackerrank/time_conversion.py
+++ hackerrank/time_conversion.py
@@ -12,7 +12,7 @@ import sys
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
 #
-def put_hms_in_arr(s):
+def put_hms_in_arr(s):#ne maha poslednite ['','']
     hms = ''
     arr = []
     for a in s:
@@ -21,33 +21,50 @@ def put_hms_in_arr(s):
         else:
             arr.append(hms)
             hms = ''
+    if hms == '':
+        arr.append(hms)
+
+    return arr #[hh, mm, ss]
+
+def is_time_pm(s):
+    for a in s:
+        if a == 'P':
+            return True
+    return False
+        
+
+def fix_time(hh, mm, ss, is_pm):# TODO,  pm 0 do 11 stava +12, ako am 12 = 00
+    hh = int(hh)
+    mm = int(mm)
+    ss = int(ss)
+    
+    if is_pm:
+        if hh != 12:
+            hh = hh + 12
+    else:
+        if hh == 12:
+            hh='00'#int->str?
             
-    arr.append(hms)  
-    return arr
-
-
+    return hh, mm, ss
+    
+    
 def timeConversion(s):
+    
     arr = put_hms_in_arr(s)
+    print('arr=', arr)
     hh = arr[0]
     mm = arr[1]
     ss = arr[2]
+
+    is_pm = is_time_pm(s) # TODO
+    print('is_pm=',is_pm)
+
+    hh, mm, ss = fix_time(hh,mm,ss, is_pm) 
     
-    is_pm = False
     
-    for x in s:    
-            
-        if x == 'P':
-            is_pm = True
-        
-        if is_pm:
-            if arr[0] == 12:
-                hh = 00
-            else:
-                hh = hh + 12
     print(hh,':', mm, ':', ss) 
-            
-            
-            
+    
+        
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
