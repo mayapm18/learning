ce626cd653c5fec72be201108117dfa5ed69f2d1
mayapm18 <ngo.h.mai.18@gmail.com>
2025-09-30 21:28:37 +0300

    wrong answer

---
+++ hackerrank/kanga_jump.py
@@ -0,0 +1,58 @@
+#https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+
+# x1+n*v1=x2+n*v2
+# x1-x2=n*(v2-v1)
+# n = (x1-x2)/(v2-v1)
+# za n = 0
+def kangaroo(x1, v1, x2, v2):
+    if x1 == x2:
+        if v1==v2:
+            return 'YES'
+        else:
+            return 'NO'
+    else:
+        return 'NO'    
+        
+            
+    if (x1-x2)%(v2-v1) == 0 and (x1-x2)/(v2-v1) >= 0:
+        return 'YES'
+    else:
+        return 'NO'
+            
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
