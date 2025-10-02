0633d968b3c330fe23869ef9ec99ffe62bb0ce37
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-02 18:59:05 +0300

    ne raboti

--- hackerrank/migratory_bird.py
+++ hackerrank/migratory_bird.py
@@ -14,31 +14,29 @@ import sys
 #
 def find_the_same_bird(arr):
     count = 0
-    for type_b in range(6):
+    for type_b in range(1, 6):
         for a in arr:
             if a == type_b:
                 count[type_b] += 1
     return count
+
 
 def migratoryBirds(arr):
     count_birds = find_the_same_bird(arr)
     max_bird = 0
     res_bird = 0
     
-    for a in arr: 
-        if count_birds[a] > max_bird:
-            max_bird = count_birds[a]
-            res_bird = a
-        elif max_bird == count_birds[a]:
-            #the smaller int(a) stay=res_bird
-            if a < res_bird:
-                res_bird = a
+    for type_b in range(1, 6): 
+        if count_birds[type_b] > max_bird:
+            max_bird = count_birds[type_b]
+            res_bird = type_b
+        elif count_birds[type_b] == max_bird:
+            if type_b < res_bird:
+                res_bird = type_b
     
     return res_bird
-            
         
             
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
