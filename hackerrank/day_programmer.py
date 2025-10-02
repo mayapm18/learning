6d1d5a4b76d4a6b3913a6e5f87fc3b645910695d
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-02 19:48:47 +0300

    4/61 failed

---
+++ hackerrank/day_programmer.py
@@ -0,0 +1,41 @@
+#https://www.hackerrank.com/challenges/day-of-the-programmer/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dayOfProgrammer' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER year as parameter.
+#
+
+def dayOfProgrammer(year):
+    if year == 1918:
+        return '26.09.' + str(year)
+    else: 
+        if year%4 == 0: 
+            if year%400 == 0: 
+                return '12.09.' + str(year) 
+            else: 
+                if year%100 == 0: 
+                    return '13.09.' + str(year) 
+                else: 
+                    return '12.09.' + str(year) 
+        else: 
+            return '13.09.' + str(year)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
