96ccce60d795c839c93fb015727c02e1809daf87
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-28 00:33:45 +0200

    🫡

--- hackerrank/fibonacci.py
+++ hackerrank/fibonacci.py
@@ -4,11 +4,11 @@ def fibonacci(n):
         return n
     f1 = 0
     f2 = 1
-    for i in range(n): 
-        new_f = f1+f2
-        f1 = f2
-        f2 = new_f
+    for i in range(n): #no need of new_f
+        f1 = f1+f2
+        f2 = f1-f2
     return f1
     
 n = int(input())
 print(fibonacci(n))
+
