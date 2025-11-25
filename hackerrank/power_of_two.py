5eb7aac869e409814aaf8c2af4dea4de7f336a80
mayapm18 <ngo.h.mai.18@gmail.com>
2025-11-25 17:29:54 +0200

    loop end

--- hackerrank/power_of_two.py
+++ hackerrank/power_of_two.py
@@ -1,14 +1,14 @@
+#https://leetcode.com/problems/power-of-two/description/
+
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-
         if n == 1:
-            return bool(1)
-        elif n % 2 == 0:
-            while n > 1:#initiate a loop that repeats a block of code as long as a condition is true
+            return True
+        elif n>0:
+            while n > 1:
+                if n % 2 != 0:
+                    return False
                 n = n // 2
-            if n == 1:
-                return bool(1)
-            else:
-                return bool(0)
+            return True
         else:
-           return bool(0)
+           return False
