0f782c367b4f35d8d874b41b4d000be620c4bbe1
mayapm18 <ngo.h.mai.18@gmail.com>
2025-12-01 17:58:29 +0200

    direct use of while

--- hackerrank/power_of_two.py
+++ hackerrank/power_of_two.py
@@ -2,13 +2,11 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 1:
-            return True
-        elif n>0:
-            while n > 1:
-                if n % 2 != 0:
-                    return False
-                n = n // 2
-            return True
-        else:
-           return False
+        if n < 1:
+            return False
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n = n // 2
+        return True
+
